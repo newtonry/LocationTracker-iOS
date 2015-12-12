26296e81ef890e996eb923800c23683fe6c94c19
Ryan Newton <rnewton@hearsaycorp.com>
2015-12-11 20:33:37 -0800

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by Ryan Newton on 12/11/15.
+//  Copyright (c) 2015 ___rvkn___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
